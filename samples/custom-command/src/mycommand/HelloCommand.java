3e5da505565ac9e5994a78d9badd07624dc654c2
gmurray <greg_murray71@yahoo.com>
2009-03-30 22:36:39 -0700

    Added ability to create custom commands and added example.

---
+++ samples/custom-command/src/mycommand/HelloCommand.java
@@ -0,0 +1,22 @@
+package mycommand;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.protorabbit.model.impl.BaseCommand;
+
+public class HelloCommand extends BaseCommand {
+
+	@Override
+	public void doProcess(OutputStream out) throws IOException {
+		
+		String message = "hello ";
+		
+		if (params.length > 0) {
+			message += params[0];
+		}
+		out.write(message.getBytes());
+		
+	}
+
+}
