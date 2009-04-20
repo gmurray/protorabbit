ac2490ba074ae581a1d599855974112ddca852ec
gmurray <greg_murray71@yahoo.com>
2009-04-20 00:31:38 -0700

    Added missing file.

---
+++ src/org/protorabbit/model/impl/BaseContext.java
@@ -0,0 +1,38 @@
+package org.protorabbit.model.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.protorabbit.model.IContext;
+
+public  abstract class BaseContext implements IContext {
+
+    private Map<String, Object>attributes;
+    private String templateId;
+
+    public BaseContext(){
+        this.attributes = new HashMap<String, Object>();
+    }
+
+    public Object getAttribute(String key) {
+        return attributes.get(key);
+    }
+
+    public Map<String, ?> getAttributes() {
+        return attributes;
+    }
+
+    public synchronized void setAttribute(String key, Object value) {
+        attributes.put(key, value);
+
+    }
+
+    public void setTemplateId(String templateId) {
+        this.templateId = templateId;
+
+    }
+
+    public String getTemplateId() {
+        return templateId;
+    }
+}
